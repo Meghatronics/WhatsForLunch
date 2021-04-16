ca47766e85c1de4f8b7f260571796c5e5ff04c07
Emmanuel Aliyu <53065184+Meghatronics@users.noreply.github.com>
2021-04-16 13:46:32 +0100

    Implementing VM into Views

--- lib/features/see_fridge_ingredients/widgets/ingredient_list_tile.dart
+++ lib/features/see_fridge_ingredients/widgets/ingredient_list_tile.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../../../base/view_base_classes/style/style.dart';
 import '../../../base/view_base_classes/widgets/simple_text.dart';
+import '../models/ingredient_model.dart';
 
 class IngredientListTile extends StatelessWidget {
-  final String ingredient;
+  final IngredientModel ingredient;
   final bool selected;
-  final Function(String ingredient) onTap;
+  final Function(IngredientModel ingredient) onTap;
 
   const IngredientListTile({
     Key key,
@@ -17,42 +18,45 @@ class IngredientListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 56,
-      margin: const EdgeInsets.only(bottom: 16),
-      padding: const EdgeInsets.symmetric(
-        horizontal: 24,
-        vertical: 16,
-      ),
-      decoration: BoxDecoration(
-          color: backgroundWhiteColor,
-          borderRadius: BorderRadius.circular(8),
-          border: Border.all(
-            color: selected ? darkGreenColor : strokeBrownColor,
-            width: selected ? 1 : 0.5,
-          )),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Flexible(
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: SimpleText(
-                '$ingredient',
-                size: 20,
-                color: const Color(0xFF141516),
-                weight: FontWeight.w300,
+    return InkWell(
+      onTap: () => onTap(ingredient),
+      child: Container(
+        height: 56,
+        margin: const EdgeInsets.only(bottom: 16),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+          vertical: 12,
+        ),
+        decoration: BoxDecoration(
+            color: backgroundWhiteColor,
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: selected ? darkGreenColor : strokeBrownColor,
+              width: selected ? 1 : 0.5,
+            )),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Flexible(
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: SimpleText(
+                  '${ingredient.name}',
+                  size: 20,
+                  color: const Color(0xFF141516),
+                  weight: FontWeight.w300,
+                ),
               ),
             ),
-          ),
-          if (selected)
-            const Icon(
-              Icons.check,
-              color: darkGreenColor,
-              size: 24,
-            )
-        ],
+            if (selected)
+              const Icon(
+                Icons.check,
+                color: darkGreenColor,
+                size: 24,
+              )
+          ],
+        ),
       ),
     );
   }
